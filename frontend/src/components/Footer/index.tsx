d11b0e47f8444139035a538e27db084e86cd280f
yurialmeidaa <yuri1193_@hotmail.com>
2021-09-07 22:37:38 -0300

    Graficos estaticos

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
   <div className="container">
-    <p className="text-light">App desenvolvido por Yuri Almeida <a href="https://github.com/yurialmeidaa" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+    <p className="text-light">App desenvolvido por Yuri Almeida <a href="https://github.com/yurialmeidaa" target="_blank" rel="noreferrer">Yuri Almeida</a></p>
     <p className="text-light"><small><strong>Semana Spring React</strong><br/>
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
   </div>
