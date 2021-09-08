8eec8c797bcb3e4318f006eba7cedcc5a498f82a
yurialmeidaa <yuri1193_@hotmail.com>
2021-09-07 22:00:08 -0300

    Componentes básicos estáveis

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/navbar";
 
 
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
